9cc9c1535f3faef848d9589ba1a2d18a74cb1269
lucashsu95 <lucashsu95@gmail.com>
2024-12-08 19:34:28 +0800

    init

---
+++ src/main/java/com/example/asset_lend_backend/AssetLendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.asset_lend_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetLendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetLendBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asset_lend_backend/AssetLendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asset_lend_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetLendBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
